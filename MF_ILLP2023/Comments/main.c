4660e4cbd6664ea59d771a045448d5532694b5a1
Mandy Farrugia <Mandy.Farrugia.c10352@mcast.edu.mt>
2023-10-10 17:53:33 +0200

    Explained the use and importance of comments.

---
+++ MF_ILLP2023/Comments/main.c
@@ -0,0 +1,12 @@
+int main(void) {
+	/* Comments are an excellent way of explaining what your code is supposed to do. 
+	 * They serve as useful documentation especially in the industry where developers work as a team.
+	 * Employees can get sick, go on leave, die, get fired or replaced, thus it is important that code contains useful comments
+	 * so it is easy to understand and the rest of the team can take it from there, preferably without the need to reach out to the original developer. 
+	 * Comments do not have to be exhaustive or added for every line, even just a small description worth a sentence long is more than enough. */
+
+	//This is an example of a single-line comment, does not span across multiple lines.
+	
+	/* This is an example of a multi-line comment, it spans across multiple lines. 
+	 * Comments are ignored by the compiler so you can type in whatever you want. */
+}
